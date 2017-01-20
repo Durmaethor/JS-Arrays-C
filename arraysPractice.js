686a30a4af00b61e4c7eaf02a2c901d50a0404a6
Ryan Kramer <rkramer613@gmail.com>
2017-01-19 19:18:49 -0700

    one more down with help

--- arraysPractice.js
+++ arraysPractice.js
@@ -90,16 +90,21 @@ function divider(numbersArray){
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 };
-//var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
+
+var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 //Above you're given a function that will return a random number between 0 and 30.  There is also a commented out array full of numbers to help you visualize what your function will be receiving.
 
 // Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
 
-function finder() { // create a function called finder
-//get a random number with getRandomArbitrary
-  //loop through the array to see if the random number generated is in the array
-    //if it is, return true.
-    }
+function finder(numbers) { 
+ 
+  if(numbers.indexOf(getRandomArbitrary()) !== -1) {
+    return true;
+  } 
+  return false;
+} 
+  
+
 
 //Next problem
 
@@ -132,8 +137,8 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 
 // USE THE COVERBANDS EXAMPLE!
 function removeItem(myGroceryList, item) {
-
-    return myGroceryList;
+    if(myGroceryList.hasOwnProperty(item)) 
+      myGroceryList.splice(item);
     }
   
 function addItem(myGroceryList, item) {
